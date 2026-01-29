a2635440087b84f811e2dded10a6fafe6e676ed7
Muslimbek <muslimbekmuminov47@gmail.com>
2026-01-29 17:39:51 +0500

    commit done

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
-// next.config.ts
-import type { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
-
-const withNextIntl = createNextIntlPlugin();
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: false // Turbopackni o‘chirib, Webpack bilan build qiladi
+  }
 };
 
-export default withNextIntl(nextConfig);
+module.exports = nextConfig;
